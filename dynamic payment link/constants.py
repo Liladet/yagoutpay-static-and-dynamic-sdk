2f6e9cbaeab6afe5d63a0c05bbff09a46589fa87
Lidiya Alemayehu <117741597+Liladet@users.noreply.github.com>
2025-09-28 11:57:50 -0500

    Update constants.py

--- dynamic payment link/constants.py
+++ dynamic payment link/constants.py
@@ -1,6 +1,6 @@
 # src/constants.py
 class YagoutPayConstants:
-    BASE_URL_TEST = "https://uatcheckout.yagoutpay.com/ms-transaction-core-1-0"
+    BASE_URL_TEST = "url"
     DYNAMIC_LINK_ENDPOINT = "/sdk/paymentByLinkResponse"
     STATIC_LINK_ENDPOINT = "/sdk/staticQRPaymentResponse"
     AGGREGATOR_ID = "yagout"
